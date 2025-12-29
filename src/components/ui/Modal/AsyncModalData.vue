3825082b6b0a98d1344be3c0ea2d1ce5bf64f25c
dgokokonda <sunny-bvb@mail.ru>
2025-12-29 20:03:35 +0300

    add suspense example

---
+++ src/components/ui/Modal/AsyncModalData.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="modal-field" v-for="(field, key) in fields" :key="field.id">
+      <strong>User {{ key }}:</strong>
+      <div>
+        Полное имя: {{ field.fields.firstname }}
+        {{ field.fields.lastname }}
+      </div>
+      <div>Возраст: {{ field.fields.age }} Город: {{ field.fields.city }}</div>
+      <div>Должность: {{ field.fields.post }}</div>
+      <br />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const fields = ref<any[]>([]);
+
+// Асинхронная инициализация
+const loadData = async (): Promise<void> => {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  fields.value = [
+    {
+      id: 1,
+      fields: {
+        firstname: "1",
+        lastname: "1",
+        age: 12,
+        city: "1",
+        post: "11",
+      },
+    },
+    {
+      id: 2,
+      fields: {
+        firstname: "2",
+        lastname: "2",
+        age: 22,
+        city: "2",
+        post: "22",
+      },
+    },
+    {
+      id: 3,
+      fields: {
+        firstname: "3",
+        lastname: "3",
+        age: 33,
+        city: "3",
+        post: "33",
+      },
+    },
+  ];
+};
+
+// Вызываем асинхронную функцию - Suspense отследит это
+await loadData();
+</script>
--- src/components/ui/Modal/Modal.vue
+++ src/components/ui/Modal/Modal.vue
@@ -1,6 +1,11 @@
 <template>
   <Transition name="modal">
-    <div v-if="props.show" class="modal-wrapper">
+    <div
+      v-if="props.show"
+      class="modal-wrapper"
+      tabindex="0"
+      @keydown.esc="$emit('close')"
+    >
       <div class="modal-container">
         <div class="modal-header">
           <slot name="header">
@@ -9,10 +14,30 @@
         </div>
         <div class="modal-body">
           <slot name="body">Содержимое модалки</slot>
+          <Suspense
+            @pending="onPending"
+            @resolve="onResolve"
+            @fallback="onFallback"
+          >
+            <AsyncModalData />
+
+            <template #fallback>
+              <div class="loading">
+                <div class="spinner"></div>
+                Загрузка данных от Suspense...
+              </div>
+            </template>
+          </Suspense>
         </div>
         <div class="modal-footer">
           <slot name="footer">
-            <button @click="$emit('close')" class="default-btn">Закрыть</button>
+            <button
+              @click="$emit('close')"
+              class="default-btn"
+              @keydown.esc="$emit('close')"
+            >
+              Закрыть
+            </button>
           </slot>
         </div>
       </div>
@@ -20,14 +45,28 @@
   </Transition>
 </template>
 <script setup lang="ts">
+import { defineAsyncComponent } from "vue";
 interface Props {
   show: boolean;
 }
-
 const props = defineProps<Props>();
-// const emit = defineEmits<{
-//   setShow: [event: boolean];
-// }>();
+
+const onPending = () => console.log("is pending");
+const onResolve = () => console.log("resolved");
+const onFallback = () => console.log("fallback");
+
+const AsyncModalData = defineAsyncComponent({
+  loader: () => import("./AsyncModalData.vue"),
+  loadingComponent: {
+    template:
+      '<div class="loading"><div class="spinner"></div>Загрузка данных формы...</div>',
+  },
+  errorComponent: {
+    template: '<div class="error">Ошибка загрузки данных...</div>',
+  },
+  delay: 200,
+  timeout: 5000,
+});
 </script>
 <style scoped>
 .modal-wrapper {
@@ -78,5 +117,33 @@ const props = defineProps<Props>();
 .modal-leave-to .modal-container {
   -webkit-transform: scale(1.1);
   transform: scale(1.1);
+}
+
+.loading {
+  text-align: center;
+  padding: 20px;
+  color: #666;
+  display: flex;
+  align-items: center;
+}
+
+.spinner {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 3px solid #f3f3f3;
+  border-top: 3px solid #42b983;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+  margin-right: 10px;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
