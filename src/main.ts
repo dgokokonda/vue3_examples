fb4aa9ba61cb221845c1eae6f9ce1b2ea87d46e3
dgokokonda <sunny-bvb@mail.ru>
2026-01-14 14:26:43 +0300

    add click-outside directive

--- src/main.ts
+++ src/main.ts
@@ -32,4 +32,21 @@ const VueVirtualScrollerPlugin = {
 //   }
 // })
 
+app.directive("click-outside", {
+  mounted(el, binding) {
+    // Создаем обработчик
+    el.__vueClickOutside__ = (event: any) => {
+      // Если клик был вне элемента — вызываем функцию
+      if (!(el === event.target || el.contains(event.target))) {
+        binding.value(event);
+      }
+    };
+    document.body.addEventListener("click", el.__vueClickOutside__);
+  },
+  unmounted(el) {
+    document.body.removeEventListener("click", el.__vueClickOutside__);
+    el.__vueClickOutside__ = null;
+  },
+});
+
 app.use(store).use(router).use(VueVirtualScrollerPlugin).mount("#app");
